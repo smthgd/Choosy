5b39bf2b0682955a4d6945c291c4cd14beee5153
smthgd <sem060504@gmail.com>
2025-03-20 18:46:42 +0500

    Update modal window design

--- Frontend/src/components/Register/Register.tsx
+++ Frontend/src/components/Register/Register.tsx
@@ -32,6 +32,7 @@ const Register: React.FC<RegisterProps> = ({ onClose }) => {
             <div className="modal-overlay" onClick={() => onClose()}></div> {/* Затемняем фон */}
             <div className="modal"> {/* Применение класса modal */}
                 <h2>Register</h2>
+                <hr className="divider" /> {/* Горизонтальная линия */}
                 <input
                     type="text"
                     value={username}
@@ -39,19 +40,22 @@ const Register: React.FC<RegisterProps> = ({ onClose }) => {
                     placeholder="Username"
                 />
                 <input
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    placeholder="Password"
-                />
-                <input
                     type="email"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                     placeholder="Email"
                 />
-                <button onClick={handleRegister}>Register</button>
-                <button onClick={onClose}>Close</button> {/* Кнопка для закрытия модального окна */}
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Password"
+                />
+                <hr className="divider" /> {/* Горизонтальная линия */}
+                <div className="button-container"> {/* Контейнер для кнопок */}
+                    <button onClick={onClose}>Close</button> {/* Кнопка для закрытия модального окна */}
+                    <button onClick={handleRegister}>Register</button> {/* Кнопка для регистрации */}
+                </div>
             </div>
         </>
     );
